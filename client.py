82d783210ae09f0ac52d168aac10e6b8f0c7b53e
tikvamooy <tikvamooy@gmail.com>
2015-09-29 13:58:08 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,52 +1,15 @@
-# chat_client.py
+import sys
+import socket
 
-import sys, socket, select
- 
-def chat_client():
-    if(len(sys.argv) < 3) :
-        print 'Usage : python chat_client.py hostname port'
-        sys.exit()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 12000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-     
-    # connect to remote host
-    try :
-        s.connect((host, port))
-    except :
-        print 'Unable to connect'
-        sys.exit()
-     
-    print 'Connected to remote host. You can start sending messages'
-    sys.stdout.write('[Me] '); sys.stdout.flush()
-     
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-         
-        for sock in read_sockets:            
-            if sock == s:
-                # incoming message from remote server, s
-                data = sock.recv(4096)
-                if not data :
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else :
-                    #print data
-                    sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()     
-            
-            else :
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('[Me] '); sys.stdout.flush() 
 
-if __name__ == "__main__":
-
-    sys.exit(chat_client())
+while True:
+    message = raw_input ("ketik pesan1: ")
+    print >>sys.stderr, 'sending "%s"' % message
+    sock.sendall(message)
+    data = sock.recv(512)
+    print >>sys.stderr, 'received "%s"' % data
